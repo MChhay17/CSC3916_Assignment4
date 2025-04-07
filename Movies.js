4f2dca826399d551e3bff6d451678dd61d1e76ef
Meme Chhay <meme.chhay@ucdenver.edu>
2025-04-06 22:59:01 -0600

    updated movie

--- Movies.js
+++ Movies.js
@@ -1,26 +1,36 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt-nodejs');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-mongoose.Promise = global.Promise;
+// Define actor subdocument schema
+const ActorSchema = new Schema({
+  actorName: { type: String, required: true },
+  characterName: { type: String, required: true }
+}, { _id: false }); // prevent _id field on each actor object
 
-try {
-    mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
-        console.log("connected"));
-}catch (error) {
-    console.log("could not connect");
-}
-mongoose.set('useCreateIndex', true);
-
-// movies schema
-var MovieSchema = new Schema({
-    title: {type: String, required: true},
-    year: {type: String, required: true},
-    genre: {type: String, required: true, enum:['Action', 'Adventure',  'Comedy',  'Drama',  'Fantasy',  'Horror',  'Mystery',  'Thriller', 'Western'] },
-    actors: {type: Array, required: true, items: {actorName: String, characterName: String}, minItems: 3},
-    imageURL: {type: String, required: true}
-
+// Movie schema
+const MovieSchema = new Schema({
+  title: { type: String, required: true },
+  year: { type: String, required: true },
+  genre: {
+    type: String,
+    required: true,
+    enum: [
+      'Action', 'Adventure', 'Comedy', 'Drama',
+      'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western'
+    ]
+  },
+  actors: {
+    type: [ActorSchema],
+    required: true,
+    validate: {
+      validator: function (val) {
+        return val.length >= 3;
+      },
+      message: 'A movie must have at least 3 actors.'
+    }
+  },
+  imageURL: { type: String, required: true }
 });
 
-// return the model to server
+// Export the Movie model
 module.exports = mongoose.model('Movie', MovieSchema);
